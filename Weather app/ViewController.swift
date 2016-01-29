ecba7e279edc4943728d93c11d92ba7dbca5146b
student <student@students-mac.student.georgianc.on.ca>
2016-01-29 09:18:11 -0500

    Initial Commit

---
+++ Weather app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather app
+//
+//  Created by student on 2016-01-29.
+//  Copyright © 2016 Adventure Programming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
